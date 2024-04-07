3f31bc243c49dd62575cfcdff987f13a359dde1d
Razique20 <raziquemk2002@gmail.com>
2024-04-08 02:38:01 +0530

    added search component

---
+++ src/api.js
@@ -0,0 +1,12 @@
+ export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+
+    
+		'X-RapidAPI-Key': 'a74df15e5amsh4c4d366b81022a9p1d333fjsnd5b8c2e24baf',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
